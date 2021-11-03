1ee4b352768d51ba715edcc0415c984b8cfc4b64
Zhaocheng Du <35942558+ZhaochengD@users.noreply.github.com>
2021-11-03 16:40:40 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from Lanechange import LaneChangeEnv
-env = LaneChangeEnv('data/cubed.csv','data/cubed_below.csv',50,50,10,20,5, 5, 0.3)
-env.reset()
-i = 0
-while(1):
-    env.step(1)
-    env.render()
-    i += 1
